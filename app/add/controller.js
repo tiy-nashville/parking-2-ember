73c0ea6e898a3bd5509f5628b77a35fa6216cfed
Nick Trull <nick.trull@gmail.com>
2016-04-23 10:11:02 -0500

    clean slate

---
+++ app/add/controller.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  addParking(attrs) {
+    const parking = {
+      street: attrs.street,
+      city: attrs.city,
+      state: attrs.state,
+    };
+
+    console.log(parking)
+
+    fetch(`http://22c6fc86.ngrok.io/addresses`, {
+      method: `POST`,
+      headers: {
+        Accept: `application/json`,
+        'Content-Type': `application/json`,
+      },
+      body: JSON.stringify(parking),
+    });
+  },
+});
