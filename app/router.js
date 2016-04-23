d9674a67641e10748a0cda112b194904341dbb25
Memphis796 <memphis796@Hotmail.com>
2016-04-23 17:33:52 -0500

    added styling

--- app/router.js
+++ app/router.js
@@ -2,10 +2,16 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
 });
 
-Router.map(function() {
+Router.map(function () {
+  this.route(`login`);
+  this.route(`home`, {path: `/`});
+
+  this.route(`about`);
+  this.route(`contact`);
+  this.route('login');
 });
 
 export default Router;
