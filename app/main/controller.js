f794cc18eb24c987f75079064a116fe3ce214717
Nick Trull <nick.trull@gmail.com>
2016-04-23 10:23:33 -0500

    store default coords as variables in template

--- app/main/controller.js
+++ app/main/controller.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   addresses: [],
+  lat: 36.1627,
+  lng: -86.7816,
 
   search(address) {
     this.set(`addresses`, [...this.addresses, address]);
